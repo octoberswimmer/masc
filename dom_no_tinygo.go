35d43c27bcf0c02664a660247e921f8111929c29
Christian G. Warden <cwarden@xerus.org>
2025-05-09 14:31:16 -0500

    Make Test Detection More Robust

--- dom_no_tinygo.go
+++ dom_no_tinygo.go
@@ -3,8 +3,15 @@
 
 package masc
 
+import (
+	"flag"
+	"os"
+	"strings"
+)
+
 func init() {
-	if isTest {
+	// skip browser/TinyGo guard when running under `go test` or other test binaries
+	if isTest || flag.Lookup("test.v") != nil || (len(os.Args) > 0 && (strings.HasSuffix(os.Args[0], ".test") || strings.HasSuffix(os.Args[0], ".test.exe"))) {
 		return
 	}
 	if global() == nil {
